d4032b7384fca16c9bc13eea61296c0ffee20b4f
Jelo777 <junheef1@gmail.com>
2024-04-09 17:45:41 +0900

    자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,26 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. Stirng Type
+let myName = "김준희";
+let myLocation = "강서구";
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. NUll type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Types
+let none;
+console.log(none);
